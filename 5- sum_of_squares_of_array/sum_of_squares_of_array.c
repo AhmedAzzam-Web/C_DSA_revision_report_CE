ee1ce9ba793b707cb218b103e544bd602ab41530
Ahmed Azzam <ahmedazzam.business@gmail.com>
2025-04-26 23:39:10 +0300

    Completed question 5 sum of squares of array elements

---
+++ 5- sum_of_squares_of_array/sum_of_squares_of_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        sum += arr[i] * arr[i];
+    }
+    printf("Sum of squares of the array is: %d\n", sum);
+    
+    // The sum of squares of the array is: 385
+}
